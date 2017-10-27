6ef28fba85f404bf504965a60469eafde08d83d7
atharkalam <athar.vds8@gmail.com>
2017-10-27 03:14:22 -0700

    Create genres.js

---
+++ Code/Mohammed Athar/Week 5/models/genres.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var genreSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    create_date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+var Genre = module.exports = mongoose.model('Genre', genreSchema);
+
+//Get Genres
+module.exports.getGeners = function(callback, limit){
+    Genre.find(callback).limit(limit);
+}
