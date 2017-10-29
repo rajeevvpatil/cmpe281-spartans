a1eb7f923f1762b7b7a501b787a23ff9699c1f11
desusaiteja <desu.saiteja@gmail.com>
2017-10-28 21:50:26 -0700

    Code changes

--- Code/Saiteja Desu/app.js
+++ Code/Saiteja Desu/app.js
@@ -1,7 +1,41 @@
+var mongoose = require('mongoose');
+var express = require('express');
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function (req, res) {
     displayForm(res);
+});
+
+var app = express();
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+mongoose.connect('UserLog:http://localhost:27017/UserLog');
+
+
+var Schema = new mongoose.Schema({
+    firstname : String,
+    lastname  : String,
+    email     : String,
+    createpswd: String,
+    confirmpswd: String
+});
+
+var user = mongoose.model('user',Schema);
+
+    app.post('/Registration',function (req,res) {
+    new user({
+        firstname : req.body.firstname,
+        lastname  : req.body.lastname,
+        email : req.body.email,
+        createpswd : req.body.createpswd,
+        confirmpswd : req.body.confirmpswd
+    }).save(function (err,doc) {
+        if(err) res.json(err);
+        else   res.send('Successfully inserted!!');
+    });
 });
 
 function displayForm(res) {
