f45ce0e67337ff495ed07d278faf02c87ca156c4
desusaiteja <desu.saiteja@gmail.com>
2017-11-02 18:26:58 -0700

    Added Sign up page Code and supported Router page

--- Code/Saiteja Desu/routes/signup.js
+++ Code/Saiteja Desu/routes/signup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Game Cart' });
+    res.render('signup', { title: 'Sign Up Page:' });
 });
 
 module.exports = router;
