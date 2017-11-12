4ee51bbb4f62f1c8d087fc20730c6dd4981ee185
AnirudhDevulapalli <danirudh2017@gmail.com>
2017-11-11 22:23:20 -0800

    Update demo_fs_update.js

--- Code/Rajeev/demo_fs_update.js
+++ Code/Rajeev/demo_fs_update.js
@@ -3,4 +3,17 @@ var fs = require('fs');
 fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
   if (err) throw err;
   console.log('Updated!');
+  http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
 }); ; 
