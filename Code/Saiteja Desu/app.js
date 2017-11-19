289ecdb8751815f3501a62384f1bf44da35f2c57
desusaiteja <desu.saiteja@gmail.com>
2017-11-18 22:20:19 -0800

    updated app.js in my module

--- Code/Saiteja Desu/app.js
+++ Code/Saiteja Desu/app.js
@@ -16,7 +16,7 @@ var session      = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/UserLog');
 
-//var index = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 //var login = require('./routes/login');
 var signup = require('./routes/signup');
@@ -81,7 +81,7 @@ app.use(function (req,res,next) {
 
 
 
-app.use('/', signup);
+app.use('/', index);
 app.use('/users', users);
 //app.use('/login', login);
 app.use('/signup', signup);
@@ -102,13 +102,13 @@ app.use(function(req, res, next) {
 */
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
